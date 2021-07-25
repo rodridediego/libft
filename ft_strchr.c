15535fabbefabee2a0dd3f23077893bcff8f2677
Your Name <you@example.com>
2021-07-25 12:31:19 +0200

    Prueba

---
+++ ft_strchr.c
@@ -0,0 +1,14 @@
+ #include "libft.h"
+ char *ft_strchr(const char *s, int c)
+ {
+     int i;
+     
+     i = 0;
+     while (s[i] != '\0')
+     {
+         if (s[i] == c)
+            return (s + i);
+        i++;
+     }
+     return (NULL);
+ }
---
+++ ft_strrchr.c
@@ -0,0 +1,19 @@
+ #include "libft.h"
+ char *ft_strrchr(const char *s, int c)
+ {
+     int i;
+     int flag;
+
+     i  = 0;
+     flag = - 1;
+     while (s[i] != '\0')
+     {
+         if (s[i] == c)
+            flag = i;
+        i++;
+     }
+     if (flag = -1)
+        return (NULL);
+     else
+        return (s + i);//si es el caracter nulo o si es alguno otro
+ }
