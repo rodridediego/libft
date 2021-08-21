ff59e00e927bd23d0e9e8a3098fbe30a2f76e5fd
Your Name <you@example.com>
2021-08-21 13:16:19 +0200

    casicasi3

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -27,7 +27,6 @@
 	}
 	return (0);
 }*/
-
 char	*ft_strnstr(const char	*big, const char *little, size_t len)
 {
 	size_t	i;
@@ -36,10 +35,8 @@ char	*ft_strnstr(const char	*big, const char *little, size_t len)
 	i = 0;
 	if (little[0] == '\0')
 		return ((char *) big);
-	if (!little || !big)
-		return (0);
 	len_l = ft_strlen(little);
-	while((*big != '\0') && (i <= len - len_l))
+	while ((*big != '\0') && (i <= len - len_l))
 	{
 		if (strncmp((const char *) big, little, len_l) == 0)
 			return ((char *) big);
