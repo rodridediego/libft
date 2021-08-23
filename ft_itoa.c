d6736cc5c486ab3ff75f8dfa1925f40c29b539ce
Your Name <you@example.com>
2021-08-23 15:51:41 +0200

    ces't finni

--- ft_itoa.c
+++ ft_itoa.c
@@ -35,11 +35,11 @@ static char	*ft_itoa_aux(int n, int sign)
 		return (num);
 	}
 	i = ft_itoa_size(ft_itoa_abs(n));
-	num = (char *) malloc(sizeof(char) * (i + 1 + sign));
+	num = (char *) malloc(sizeof(char) * (i + sign + 1));
 	if (num == NULL)
 		return (NULL);
 	num[i + sign] = '\0';
-	while (i >= 0)
+	while (i > 0)
 	{
 		aux = ft_itoa_abs(n) % 10;
 		n = ft_itoa_abs(n) / 10;
@@ -62,54 +62,3 @@ char	*ft_itoa(int n)
 		num[0] = '-';
 	return (num);
 }
-/*
-static size_t	ft_intlen(long nb)
-{
-	size_t	count;
-
-	count = 0;
-	if (nb == 0)
-	{
-		count++;
-		return (count);
-	}
-	if (nb < 0)
-	{
-		nb = nb * -1;
-		count++;
-	}
-	while (nb > 0)
-	{
-		nb = nb / 10;
-		count++;
-	}
-	return (count);
-}
-
-char	*ft_itoa(int n)
-{
-	char	*strnum;
-	size_t	len;
-	long	nb;
-
-	nb = n;
-	len = ft_intlen(nb);
-	strnum = (char *)malloc(sizeof(char) * len + 1);
-	if (!strnum)
-		return (NULL);
-	strnum[len--] = '\0';
-	if (n == 0)
-		strnum[0] = '0';
-	if (nb < 0)
-	{
-		strnum[0] = '-';
-		nb = nb * -1;
-	}
-	while (nb > 0)
-	{
-		strnum[len] = (nb % 10) + '0';
-		nb = nb / 10;
-		len--;
-	}
-	return (strnum);
-}*/
